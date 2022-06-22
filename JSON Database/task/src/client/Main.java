0ea6efd45a95e47dcaefb621606cf985e6f865b6
Jed <jedwalton98@gmail.com>
2022-06-22 04:39:14 +0100

    Stage 2/6: Network connection

--- JSON Database/task/src/client/Main.java
+++ JSON Database/task/src/client/Main.java
@@ -1,14 +1,29 @@
 package client;
 
-import server.DatabaseController;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        DatabaseController databaseController = new DatabaseController();
+        String address = "127.0.0.1";
+        int port = 23456;
+        Socket socket = new Socket(InetAddress.getByName(address), port);
+        System.out.println("Client started!");
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
+
+        String toSendToServer = "Give me a record of # N";
+        System.out.println("Client started!");
+        System.out.print("Sent: " + toSendToServer);
+        output.writeUTF(toSendToServer);
+        System.out.println("Received: " + input.readUTF());
     }
 
 }
--- JSON Database/task/src/server/Main.java
+++ JSON Database/task/src/server/Main.java
@@ -1,10 +1,34 @@
 package server;
 
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DatabaseController databaseController = new DatabaseController();
+
+
+        String address = "127.0.0.1";
+        int port = 23456;
+        ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));
+        System.out.println("Server started!");
+        Socket socket = server.accept();
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output  = new DataOutputStream(socket.getOutputStream());
+
+
+        String readUTFInput = input.readUTF();
+        String serverResponse = "A record # N was sent!";
+        System.out.println("Received: " + readUTFInput);
+        System.out.print("Sent: " + serverResponse);
+        output.writeUTF(serverResponse);
+
         databaseController.processUserInput();
     }
 }
